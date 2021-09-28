e5fa2bac7f8644e0ae8c61f0051e1529358a97b0
Huy Ha <Huy Ha@DESKTOP-FTC217R>
2021-09-28 15:35:44 +0700

    Add project files.

---
+++ bUnitTest/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace bUnitTest
+{
+    public class Class1
+    {
+    }
+}
---
+++ bUnitTest/CounterTest.cs
@@ -0,0 +1,21 @@
+﻿using Bunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bUnitTest
+{
+    public class CounterTest
+    {
+        private readonly TestContext _testContext;
+
+        public CounterTest()
+        {
+            _testContext = new TestContext();
+        }
+
+        
+    }
+}
