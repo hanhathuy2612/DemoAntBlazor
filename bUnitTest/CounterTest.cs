bfa9cfac82dac1fad135050711f6bb68ac20e8c0
Huy Ha <Huy Ha@DESKTOP-FTC217R>
2021-09-29 14:06:53 +0700

    push

--- bUnitTest/CounterTest.cs
+++ bUnitTest/CounterTest.cs
@@ -14,32 +14,32 @@ namespace bUnitTest
 {
     public class CounterTest
     {
-        //[Fact]
-        //public void Test()
-        //{
-        //    using var ctx = new TestContext();
+        [Fact]
+        public void Test()
+        {
+            using var ctx = new TestContext();
 
-        //    ctx.Services.AddSingleton<IAverageCountService, AverageCountService>();
-        //    ctx.RenderTree.Add<CascadingValue<int>>(para => para.Add(p => p.Value, 20));
+            ctx.Services.AddSingleton<IAverageCountService, AverageCountService>();
+            ctx.RenderTree.Add<CascadingValue<int>>(para => para.Add(p => p.Value, 20));
 
-        //    Action<int> onSomethingHandler = (e) => { e++; };
-        //    var cut = ctx.RenderComponent<Counter>(parameters =>
-        //        parameters.Add(p => p.CurrentCount, 5)
-        //                  .Add(p => p.CurrentCountChanged, onSomethingHandler)
-        //    );
-        //    var buttonElement = cut.FindAll("button");
-        //    buttonElement.FirstOrDefault(x => x.TextContent == "Click me").Click();
+            Action<int> onSomethingHandler = (e) => { e++; };
+            var cut = ctx.RenderComponent<Counter>(parameters =>
+                parameters.Add(p => p.CurrentCount, 5)
+                          .Add(p => p.CurrentCountChanged, onSomethingHandler)
+            );
+            var buttonElement = cut.FindAll("button");
+            buttonElement.FirstOrDefault(x => x.TextContent == "Click me").Click();
 
-        //    //cut.Find("h2").MarkupMatches("<h2>Counter 20</h2>");
-        //    //Assert.Equal(20, cut.Instance.CascadingValue);
-        //    //Assert.Equal(4, cut.FindComponents<Foo>().Count());
+            //cut.Find("h2").MarkupMatches("<h2>Counter 20</h2>");
+            //Assert.Equal(20, cut.Instance.CascadingValue);
+            //Assert.Equal(4, cut.FindComponents<Foo>().Count());
 
-        //    //Assert.NotNull(ctx.Services.GetService<AverageCountService>());
+            //Assert.NotNull(ctx.Services.GetService<AverageCountService>());
 
-        //    //Assert.NotEmpty(cut.Instance._strings);
-        //    //cut.Find("button").Click();
+            //Assert.NotEmpty(cut.Instance._strings);
+            //cut.Find("button").Click();
 
-        //    //cut.Find("h2").MarkupMatches("<h2>Counter</h2>");
-        //}
+            //cut.Find("h2").MarkupMatches("<h2>Counter</h2>");
+        }
     }
 }
